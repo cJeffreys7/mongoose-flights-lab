fc272b8ae82e2599107e5e5730b989981ba913d6
ChrisJeffreys <ChrisBJeffreys@gmail.com>
2021-10-28 17:51:23 -0500

    connect to db

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+import 'dotenv/config.js'
 import createError from 'http-errors'
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+
+// Connect to the db with Mongoose
+import('./config/database.js')
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
